f03836e3240cb348a446440f831f590bed5db59d
Nguyet Doan <minhnguyetdoan057@gmail.com>
2021-09-25 14:04:25 +0700

    change something

--- src/containers/Aboutme/AboutMe.js
+++ src/containers/Aboutme/AboutMe.js
@@ -10,9 +10,9 @@ const AboutMe = () => {
     animatedClassName: "animated",
     duration: 2000,
   });
+  let arrName =
+    "My name is Nguyet. I'm trying to be a great Frontend Developer.";
   useEffect(() => {
-    let arrName = "My name is Nguyet. I'm a Frontend Developer.";
-
     const setIn = setTimeout(() => {
       let name = "";
       for (let i = 0; i < arrName.length; i++) {
@@ -23,10 +23,8 @@ const AboutMe = () => {
       setTimeout(() => setIntro(name), arrName.length * 200);
     });
     return () => clearTimeout(setIn);
-  }, []);
+  }, [arrName]);
   useEffect(() => {
-    let arrName = "My name is Nguyet. I'm a Frontend Developer.";
-
     const setIn = setInterval(() => {
       let name = "";
       for (let i = 0; i < arrName.length; i++) {
@@ -36,48 +34,53 @@ const AboutMe = () => {
       }
       setTimeout(() => setIntro(name), arrName.length * 200);
     }, (arrName.length + 1) * 200);
-    return () => clearTimeout(setIn);
-  }, []);
+    return () => clearInterval(setIn);
+  }, [arrName]);
 
   return (
     <div className={classes.container} data-aos="fade-up">
       <div className={classes.introduction}>
-        <div className={classes.text}>
+        <div>
           <h1>Hello,</h1>
           <h1>{intro}</h1>
         </div>
-        <ul>
-          <li>
-            <a
-              href="https://www.facebook.com/nguyetthoicute/"
-              target="_blank"
-              rel="noreferrer"
-            >
-              <i className="fab fa-facebook"></i>
-            </a>
-          </li>
+        <div className={classes["contact"]}>
+          <h2>Contact me:</h2>
+          <ul>
+            <li>
+              <a
+                href="https://www.facebook.com/nguyetthoicute/"
+                target="_blank"
+                rel="noreferrer"
+              >
+                <i className="fab fa-facebook"></i>
+              </a>
+            </li>
 
-          <li>
-            <a
-              href="https://github.com/nguyetdoan"
-              target="_blank"
-              rel="noreferrer"
-            >
-              <i className="fab fa-github"></i>
-            </a>
-          </li>
-          <li>
-            <a
-              href="https://github.com/nguyetdoan"
-              target="_blank"
-              rel="noreferrer"
-            >
-              <i className="fab fa-linkedin"></i>
-            </a>
-          </li>
-        </ul>
+            <li>
+              <a
+                href="https://github.com/nguyetdoan"
+                target="_blank"
+                rel="noreferrer"
+              >
+                <i className="fab fa-github"></i>
+              </a>
+            </li>
+            <li>
+              <a
+                href="https://github.com/nguyetdoan"
+                target="_blank"
+                rel="noreferrer"
+              >
+                <i className="fab fa-linkedin"></i>
+              </a>
+            </li>
+          </ul>
+        </div>
       </div>
-      <img src="assets/avt.png" alt="avatar" />
+      <div class={classes["image-container"]}>
+        <img src="assets/avt.png" alt="avatar" />
+      </div>
     </div>
   );
 };
